c13002f8086e10a4b0be3e96f22cf30790c70e62
AlVielma <vielma7220@gmail.com>
2024-04-03 13:40:01 -0600

    chingodecosas

--- dealornotdeal/JuegoViewController.swift
+++ dealornotdeal/JuegoViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 class JuegoViewController: UIViewController {
     
     
-
+    
     @IBOutlet var maletines: [UIButton]!
+    
+    @IBOutlet var contador: UILabel!
+    
+    @IBOutlet var contadoround: UILabel!
+    
+    
+    @IBOutlet var precios: [UILabel]!
+    
+    let precioss = [1, 10, 50, 100, 200, 400, 500, 1000, 5000, 10000, 50000, 100000, 200000, 400000, 500000, 1000000]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +29,5 @@ class JuegoViewController: UIViewController {
     }
     
 
-    @IBAction func regresar() {
-        dismiss(animated: true)
-    }
+   
 }
