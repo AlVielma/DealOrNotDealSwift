1ccc1d483ac328dea1580d9a99c0f2e0b786d09b
AlVielma <carlos.avalos0409@gmail.com>
2024-04-18 19:15:56 -0600

    ajas

--- dealornotdeal/AppDelegate.swift
+++ dealornotdeal/AppDelegate.swift
@@ -12,42 +12,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //abrirArchivo()
+        crearUsuariosAleatorios()
+        abrirArchivo()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    // MARK: - Crear usuarios aleatorios y guardarlos en el archivo plist
     
-    func abrirArchivo()
-    {
-        let datos = UserData.sharedUserData()
-        let ruta = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) [0] + "/Config.plist"
-    let urlArchivo = URL(fileURLWithPath: ruta)
-        do
-            {
-                let archivo = try Data.init(contentsOf: urlArchivo)
-                let diccionario = try PropertyListSerialization.propertyList(from: archivo, format: nil) as! [String:Any]
-                
-                datos.nombre = diccionario["nom"] as! String
-                datos.record = diccionario["rec"] as! Float
+    func crearUsuariosAleatorios() {
+        var usuarios: [[String:Any]] = []
 
+        for i in 1...5 {
+            let nombre = "Usuario \(i)"
+            let record = Float.random(in: 0.0...100.0)
+            let usuario: [String:Any] = ["nombre": nombre, "record": record]
+            usuarios.append(usuario)
+        }
+
+        guardarArchivo(diccionario: usuarios)
+    }
+
+    // MARK: - Métodos de archivo
+
+    func abrirArchivo() {
+        let datos = UserData.sharedUserData()
+        let ruta = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/Config.plist"
+        let urlArchivo = URL(fileURLWithPath: ruta)
+        
+        do {
+            let archivo = try Data(contentsOf: urlArchivo)
+            let diccionario = try PropertyListSerialization.propertyList(from: archivo, format: nil) as! [[String:Any]]
+            
+            // Actualizar los datos del usuario único en este ejemplo
+            if let usuario = diccionario.first {
+                datos.nombre = usuario["nombre"] as! String
+                datos.record = usuario["record"] as! Float
             }
-            catch
-            {
-                print("Error al leer el archivo plist")
-            }
+        } catch {
+            print("Error al leer el archivo plist")
+        }
     }
 
     func guardarArchivo(diccionario: [[String:Any]]) {
@@ -61,9 +64,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Error al guardar el archivo plist")
         }
     }
-    
-    
 
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
 
 }
-
